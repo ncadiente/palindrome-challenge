51a2d660264d7c9ef34b83a0f494d103272ffe44
Nick Cadiente <nickcadiente@Nicks-MacBook-Pro.local>
2015-12-10 10:37:51 -1000

    returns correct answer using recursion

--- palindromeGen.js
+++ palindromeGen.js
@@ -1,26 +1,18 @@
+  var steps = 0;
 
 function palindromeNumberGenerator(number) {
-  var reverse = function(number) {
-    var reversed = Number(number.toString().split('').reverse().join(''));
-    return reversed;
-  };
-
-  var steps = 0;
   var currentNumber = number;
-  var currentReverse = reverse(number);
-  var numberSteps ={};
-  if (currentNumber === currentReverse){
-    numberSteps.value = currentNumber;
-    numberSteps.steps = steps;
-    return numberSteps;
+  var currentReverse = Number(number.toString().split('').reverse().join(''));
+  if (currentNumber === currentReverse) {
+    var palindrome = {};
+    palindrome.value = currentNumber;
+    palindrome.steps = steps;
+    return palindrome;
   }
-  console.log(steps);
-  console.log(currentNumber);
-  console.log(currentReverse);
-  if (currentNumber !== currentReverse) {
+  if (currentNumber !== currentReverse){
     currentNumber+= currentReverse;
     steps++;
-    palindromeNumberGenerator(currentNumber);
+    return palindromeNumberGenerator(currentNumber);
   }
 }
 
